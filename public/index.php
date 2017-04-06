8a23d8de396ee591d7f9e32d5c29855da68df50c
Lindsay Foley <lprahalad@hotmail.com>
2017-04-06 22:06:03 +0100

    Initial commit
    WIP, almost fin homepage

---
+++ public/index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+	<title>Home | Chad Rostron | Consultant Ophthalmologist</title>
+	<?php include('structure/meta.php'); ?>
+	<meta name="description" content="Chad Rostron is a consultant eye surgeon specialising in corneal and cataract surgery, with a practice based at Harley Street, London.">
+	<?php include('structure/styles.php'); ?>
+	<?php include('structure/icons.php'); ?>
+  </head>
+  
+  <body>
+	  
+	<header>
+		<div class="top-bar" id="main-menu">
+			<div id="logo" class="large-5 column">
+				<h3 class="text-center">Chad Rostron</h3>
+			</div>
+			<div class="large-7 column">
+			    <nav class="menu-centered">
+					<ul class="vertical medium-horizontal menu">
+					  <li><a href="experience.php">Experience</a></li>
+					  <li><a href="publications.php">Publications</a></li>
+					  <li><a href="about.php">About</a></li>
+					  <li><a href="contact.php">Contact</a></li>
+					</ul>
+			    </nav>
+			</div>
+	    </div>
+		<div id="intro" class="row">
+			<h3>A Consultant Ophthalmologist dealing with diseases, injuries and operations involving the eyes such as Glaucoma, Diabetic Retinopathy and Macular Degeneration.</h3>
+		</div>	
+	</header>
+	
+	<section>
+		<main class="row">
+			<h4 class="text-center">Background &amp; Latest Work</h4>
+			<div class="large-12 column">
+				<article>
+					<div class="row">
+						<div class="large-6 column">
+							<img src="images/chad-theatre-with-assistant.jpg" alt="Chad with a patient in Linden, Guyana">
+						</div>
+						<div class="large-6 column">
+							<h5><a href="experience.php">Overseas Clinic With Jesus In Action</a></h5>
+							<p>Mr Rostron has been visiting Guyana, South America for a number of years, establishing new ophthalmic services.</p>
+							<p>Having recently arrived back from Guyana after a 4 week trip, Mr Rostron took the opportunity to do an all day clinic seeing patients in the new Jesus In Action facility in Linden.</p>
+							<a class="button small" href="experience.php">Find Out More</a>
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="large-6 column">
+							<img src="images/visual-field.jpg" alt="Visual field diagram">
+						</div>
+						<div class="large-6 column">
+							<h5><a href="experience.php">London Consulting &amp; Experience</a></h5>
+							<p>Mr Rostron is a consultant eye surgeon specialising in corneal and cataract surgery with a practice based in Harley Street, London.</p>
+							<p>He was previously Medical Director of Accuvision Laser Eye Centres and also Consultant Ophthalmologist at Moorfields Eye Department at St George's Hospital, and Honorary Senior Lecturer in Ophthalmology at St George's, University of London.</p>
+							<a class="button small" href="experience.php">Read More</a>
+						</div>
+					</div>
+				</article>
+			</div>
+		</main>
+	</section>
+	
+	<?php include('structure/footer.php'); ?>
+	
+	<?php include('structure/scripts.php'); ?>
+	
+  </body>
+</html>
---
+++ public/structure/footer.php
@@ -0,0 +1,10 @@
+<footer>
+	<div class="row">
+		<div class="large-6 column">
+			<a class="text-left" target="_blank" href="http://www.jesusinaction.co.uk">Visit Jesus In Action Charity</a>	
+		</div>	
+		<div class="large-6 column">
+			<p class="text-right">Copyright &copy; <a href="http://www.lindsayfoley.co.uk">Lindsay Foley</a></p>
+		</div>
+	</div>
+</footer>
